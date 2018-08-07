2e2dec6e65b08aab5e6b3b6b9211d37470eb6f38
Kiselevsky1 <40317306+Kiselevsky1@users.noreply.github.com>
2018-08-07 12:20:40 +0300

    dict and list

---
+++ dict$list.py
@@ -0,0 +1,49 @@
+#LIST
+L = [1,2,3,4] #Create list
+print(L)
+
+L.append(5) #add element
+print(L)
+
+L.extend([6,7,8]) #add  elements
+print(L)
+
+L.insert(8 , 8) #add 9 on index 8
+print(L)
+
+print(L.index(8)) #index elemnt ( list.index(x , [start , stop])  )
+
+print(L.count(8)) # Count element
+
+L.reverse() # sort list 1 < 0
+print(L)
+
+L.sort() # sort list 0 > 1
+print(L)
+
+L.pop(8) # delete elemnt on index
+print(L)
+
+L.remove(1) # delite on value
+print(L)
+
+
+
+del L[1]
+print(L)
+del L[5:8]
+print(L)
+
+
+
+L = [x**2 for x in range(5)]
+print(L)
+
+print(list(map(ord, "spam")))
+
+
+
+
+
+
+
